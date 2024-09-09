a4e3ce19d55d13286c79ef17aa2775eb9e17ba04
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-09-09 14:47:25 -0400

    Inject HousingService

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -10,17 +10,37 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   standalone: true,
   imports: [],
   template: `
-    <p>
-      details works! {{ housingLocationId }}
-    </p>
+    <article>
+      <img
+        class="listing-photo"
+        [src]="housingLocation?.photo"
+        alt="Exterior photo of {{ housingLocation?.name }}"
+        crossorigin
+      />
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+        <p class="listing-location">{{ housingLocation?.city }}, {{ housingLocation?.state }}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Units available: {{ housingLocation?.availableUnits }}</li>
+          <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
+          <li>Does this location have laundry: {{ housingLocation?.laundry }}</li>
+        </ul>
+      </section>
+    </article>
   `,
   styles: ``
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-         housingLocationId = -1;
-         constructor() {
-             this.housingLocationId = Number(this.route.snapshot.params['id']);
-         }
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
+  
+  constructor() {
+  const housingLocationId = Number(this.route.snapshot.params['id']);
+  this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
 
 }
